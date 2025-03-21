28f5a64ec82e96a73180e6812e9a3fe4017b2b03
ChandrikaDavid <davidchandrika21@gmail.com>
2025-03-21 17:05:31 +0530

    commit

--- src/test/java/MyRunner/signUpRunner.java
+++ src/test/java/MyRunner/signUpRunner.java
@@ -9,7 +9,7 @@ import base.BaseClass;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-//@RunWith(Cucumber.class)
+
 @CucumberOptions(features = ".\\src\\main\\java\\features\\signup.feature",
 	glue = { "stepDefinations","hooks"},
 	plugin = { "pretty","html:target/cucumber-reports.html"}, 
@@ -23,19 +23,3 @@ public class signUpRunner extends AbstractTestNGCucumberTests {
 	}
 }
 
-//package MyRunner;
-//
-//import org.junit.runner.RunWith;
-//
-//import io.cucumber.junit.Cucumber;
-//import io.cucumber.junit.CucumberOptions;
-//
-//@RunWith(Cucumber.class)
-//@CucumberOptions(features = ".\\src\\main\\java\\features\\signup.feature",
-//	glue = { "stepDefinations",	"hooks" },
-//	plugin = { "pretty" },
-//	tags = "@signup",
-//	monochrome = false)
-//public class signUpRunner{
-//
-//}
